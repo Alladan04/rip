f8b0223ddbafef0e3ebf91daf831dbae3ace2d25
Alladan04 <alla.danielyan.2004@mail.ru>
2023-09-23 13:16:39 +0300

    design

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -5,9 +5,9 @@ from django.shortcuts import render
 from datetime import date
 
 data ={ 'data':{'orders': [
-    {'id':1, 'title':'Сложение','img':'https://pngimg.com/uploads/plus/plus_PNG31.png','text':'some text', 'type':'Арифметический','price':123.0},
-    {'id':2, 'title':'Вычитание', 'img':'https://cdn.icon-icons.com/icons2/1144/PNG/512/subtractsign_80955.png', 'text':'some text', 'type':'Арифметический','price':123.0},
-    {'id':3, 'title':'Умножение', 'img':'https://cdn-icons-png.flaticon.com/512/1/1659.png','text':'some text','type':'Арифметический', 'price':10.0},
+    {'id':1, 'title':'Сложение','img':'https://pngimg.com/uploads/plus/plus_PNG31.png','text':'Сложе́ние — одна из основных бинарных математических операций двух аргументов, результатом которой является новое число, получаемое увеличением значения первого аргумента на значение второго аргумента.', 'type':'Арифметический','price':123.0},
+    {'id':2, 'title':'Вычитание', 'img':'https://cdn.icon-icons.com/icons2/1144/PNG/512/subtractsign_80955.png', 'text':'Вычита́ние — одна из вспомогательных бинарных математических операций двух аргументов, результатом которой является новое число, получаемое уменьшением значения первого аргумента на значение второго аргумента. ', 'type':'Арифметический','price':123.0},
+    {'id':3, 'title':'Умножение', 'img':'https://cdn-icons-png.flaticon.com/512/1/1659.png','text':'Умноже́ние — одна из основных математических операций над двумя аргументами, которые называются множителями или сомножителями. ','type':'Арифметический', 'price':10.0},
     {'id':4, 'title':'Деление', 'img':'https://cdn-icons-png.flaticon.com/512/660/660236.png', 'text':'some text','type':'Арифметический', 'price':5.0},
     {'id':5, 'title':'XOR', 'img':'https://static.thenounproject.com/png/711172-200.png','text':'sometext','type':'Логический', 'prcie':12.0}
     ]}}
