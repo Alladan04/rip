26631395656e83d7c055bf68912078aff79037e0
Alladan04 <alla.danielyan.2004@mail.ru>
2024-01-22 05:01:40 +0300

    deleted something

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    #'ops',
+    
     'rest_framework',
     'drf_yasg',
 ]
@@ -89,7 +89,7 @@ CORS_ALLOWED_ORIGINS = [
 "https://alladan04.github.io",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+#CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 ROOT_URLCONF = 'myproject.urls'
 
